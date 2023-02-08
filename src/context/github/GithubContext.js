cdac06c19d213babe3f46228f1c565b836a85336
Sandro Simic <sandrosimic8@gmail.com>
2023-02-08 11:27:06 +0100

    Final Commit

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -11,6 +11,7 @@ export const GithubProvider = ({children}) => {
     const initialState = {
         users: [],
         user: {},
+        repos: [],
         loading: false
     }
 
@@ -64,6 +65,31 @@ export const GithubProvider = ({children}) => {
     }
     }
 
+    // Get User Repos
+    const getUserRepos = async (login) => {
+        setLoading()
+
+        
+        const params = new URLSearchParams({
+            sort: 'created',
+            per_page:10,
+        })
+
+        const response = await fetch(`${GITHUB_URL}/users/${login}/repos?${params}`,
+        { 
+        headers:{
+            Authorization: `token ${GITHUB_TOKEN}`
+        }
+    })
+
+        const data = await response.json();
+
+        dispatch({
+            type: 'GET_REPOS',
+            payload: data,
+        })
+    }
+
 
     // Clear users from state
     const clearUsers = () => dispatch({type: 'CLEAR_USERS'})
@@ -72,7 +98,7 @@ export const GithubProvider = ({children}) => {
 
     const setLoading = () => dispatch({type: 'SET_LOADING'})
 
-    return <GithubContext.Provider value={{users: state.users,loading: state.loading,user:state.user,searchUsers, clearUsers, getUser}}>
+    return <GithubContext.Provider value={{users: state.users,loading: state.loading,user:state.user, repos:state.repos,searchUsers, clearUsers, getUser, getUserRepos}}>
         {children}
     </GithubContext.Provider>
 }
